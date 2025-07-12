f29d63a3ebbc9311475f92ec413e03b54a3862a3
sats0264 <sats0264@gmail.com>
2025-07-12 16:04:39 +0000

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module location.app.vehicule_location_app {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens location.app.vehicule_location_app to javafx.fxml;
+    exports location.app.vehicule_location_app;
+}
