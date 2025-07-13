ddf12c627abc2bcff447e4a085e66609b9406870
sats0264 <sats0264@gmail.com>
2025-07-13 17:37:49 +0000

    Observer

---
+++ src/main/java/location/app/vehicule_location_app/observer/Observer.java
@@ -0,0 +1,7 @@
+package location.app.vehicule_location_app.observer;
+
+public abstract class Observer {
+
+    protected Subject subject;
+    public abstract void update();
+}
---
+++ src/main/java/location/app/vehicule_location_app/observer/Subject.java
@@ -0,0 +1,22 @@
+package location.app.vehicule_location_app.observer;
+
+import java.util.ArrayList;
+
+public class Subject{
+
+    private ArrayList<Observer> observers = new ArrayList<Observer>();
+    private int state;
+    public int getState() { return state; }
+    public void setState(int state) {
+        this.state = state;
+        notifyAllObservers();
+    }
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+    public void notifyAllObservers(){
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+}
