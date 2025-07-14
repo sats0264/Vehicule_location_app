9c95267a3400acebb2c7c5c3f7e23d9c252f7616
sats0264 <sats0264@gmail.com>
2025-07-14 11:17:47 +0000

    Security

--- src/main/java/location/app/vehicule_location_app/dao/HibernateObjectDaoImpl.java
+++ src/main/java/location/app/vehicule_location_app/dao/HibernateObjectDaoImpl.java
@@ -17,61 +17,94 @@ public class HibernateObjectDaoImpl<T> implements IDao<T> {
 
     @Override
     public void create(T entity) throws DAOException {
-        try {
-            Session session = HibernateConnection.getInstance().getSession();
-            Transaction transaction = session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = HibernateConnection.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
             session.persist(entity);
             transaction.commit();
         } catch (Exception e) {
-            throw new DAOException("ERROR : " + e.getClass() + " : " + e.getMessage());
+            if (transaction != null) transaction.rollback();
+            throw new DAOException("Erreur création : " + e.getMessage());
         }
     }
 
     @Override
     public T read(int id) throws DAOException {
-        try {
-            Session session = HibernateConnection.getInstance().getSession();
+        try (Session session = HibernateConnection.getSessionFactory().openSession()) {
             return session.find(type, id);
         } catch (Exception e) {
-            throw new DAOException("ERROR : " + e.getClass() + ":" + e.getMessage());
+            throw new DAOException("Erreur lecture : " + e.getMessage());
         }
     }
 
     @Override
     public List<T> list() throws DAOException {
-        try {
-            Session session = HibernateConnection.getInstance().getSession();
-            String query = "from " + type.getSimpleName();
-            return session.createQuery(query, type).getResultList();
+        try (Session session = HibernateConnection.getSessionFactory().openSession()) {
+            String hql = "from " + type.getSimpleName();
+            return session.createQuery(hql, type).getResultList();
         } catch (Exception e) {
-            throw new DAOException("ERROR : " + e.getClass() + ":" + e.getMessage());
+            throw new DAOException("Erreur listing : " + e.getMessage());
         }
     }
 
     @Override
     public void update(T entity) throws DAOException {
-        try {
-            Session session = HibernateConnection.getInstance().getSession();
-            Transaction transaction = session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = HibernateConnection.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
             session.merge(entity);
             transaction.commit();
         } catch (Exception e) {
-            throw new DAOException("ERROR : " + e.getClass() + ":" + e.getMessage());
+            if (transaction != null) transaction.rollback();
+            throw new DAOException("Erreur update : " + e.getMessage());
         }
     }
 
     @Override
     public void delete(int id) throws DAOException {
-        try {
-            Session session = HibernateConnection.getInstance().getSession();
-            Transaction transaction = session.beginTransaction();
-            T entity = read(id);
-            if (entity != null) {
-                session.remove(entity);
-            }
+        Transaction transaction = null;
+        try (Session session = HibernateConnection.getSessionFactory().openSession()) {
+            T entity = session.find(type, id);
+            if (entity == null) throw new DAOException("Objet non trouvé");
+            transaction = session.beginTransaction();
+            session.remove(entity);
             transaction.commit();
         } catch (Exception e) {
-            throw new DAOException("ERROR : " + e.getClass() + ":" + e.getMessage());
+            if (transaction != null) transaction.rollback();
+            throw new DAOException("Erreur suppression : " + e.getMessage());
+        }
+    }
+
+    public int getId(T entity) throws DAOException {
+        Transaction transaction = null;
+        try (Session session = HibernateConnection.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            if (entity == null) {
+                throw new DAOException("L'entité ne peut pas être nulle");
+            }
+            Integer id = (Integer) session.getIdentifier(entity);
+            if (id == null) {
+                throw new DAOException("L'entité n'a pas d'identifiant");
+            }
+            transaction.commit();
+            return id;
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw new DAOException("Erreur récupération ID : " + e.getMessage());
+        }
+    }
+
+    public int getAllEntitiesCount() throws DAOException {
+        Transaction transaction = null;
+        try(Session session = HibernateConnection.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            String hql = "select count(*) from " + type.getSimpleName();
+            Long count = session.createQuery(hql, Long.class).uniqueResult();
+            transaction.commit();
+            return count != null ? count.intValue() : 0;
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw new DAOException("Erreur comptage des entités : " + e.getMessage());
         }
     }
 }
--- src/main/java/location/app/vehicule_location_app/jdbc/HibernateConnection.java
+++ src/main/java/location/app/vehicule_location_app/jdbc/HibernateConnection.java
@@ -1,41 +1,68 @@
+//package location.app.vehicule_location_app.jdbc;
+//
+//
+//
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.boot.Metadata;
+//import org.hibernate.boot.MetadataSources;
+//import org.hibernate.boot.registry.StandardServiceRegistry;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//
+//public class HibernateConnection {
+//    private static final HibernateConnection instance = new HibernateConnection();
+//    private final Session session;
+//    private final SessionFactory factory;
+//
+//    private HibernateConnection() {
+//        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder()
+//                .configure("hibernate.cfg.xml")
+//                .build();
+//
+//        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+//
+//        // Creating Hibernate Session Factory Instance
+//        factory = meta.getSessionFactoryBuilder().build();
+//        // Creating The Hibernate's Session Object
+//        session = factory.openSession();
+//
+//    }
+//
+//    public Session getSession() {
+//        if (session == null) {
+//            new HibernateConnection();
+//        }
+//        return session;
+//    }
+//
+//    public static HibernateConnection getInstance() {
+//        return instance;
+//    }
+//}
 package location.app.vehicule_location_app.jdbc;
 
-
-
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateConnection {
-    private static final HibernateConnection instance = new HibernateConnection();
-    private final Session session;
-    private final SessionFactory factory;
 
-    private HibernateConnection() {
-        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder()
-                .configure("hibernate.cfg.xml")
-                .build();
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
-        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
-
-        // Creating Hibernate Session Factory Instance 
-        factory = meta.getSessionFactoryBuilder().build();
-        // Creating The Hibernate's Session Object
-        session = factory.openSession();
-
-    }
-
-    public Session getSession() {
-        if (session == null) {
-            new HibernateConnection();
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError("Erreur d'initialisation de Hibernate : " + ex);
         }
-        return session;
     }
 
-    public static HibernateConnection getInstance() {
-        return instance;
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
     }
 }
