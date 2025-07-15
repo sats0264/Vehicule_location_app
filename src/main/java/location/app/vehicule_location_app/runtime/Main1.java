00415fc8fbb24a365c55417292ea43c72f3def4d
sats0264 <sats0264@gmail.com>
2025-07-15 22:35:26 +0000

    Main test 2

---
+++ src/main/java/location/app/vehicule_location_app/runtime/Main1.java
@@ -0,0 +1,25 @@
+package location.app.vehicule_location_app.runtime;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main1 extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main1.class.getResource("/views/UIlogin.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Connexion - Application de Location de Véhicules");
+        stage.setScene(scene);
+        stage.setResizable(false); // Empêche le redimensionnement pour une interface de connexion fixe
+        stage.show();
+    }
+}
