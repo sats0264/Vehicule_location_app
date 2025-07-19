414bc35bbc99b192d9d4538704abf0c1b31d9dd4
sats0264 <sats0264@gmail.com>
2025-07-19 01:26:31 +0000

    Update Observer

--- src/main/java/location/app/vehicule_location_app/observer/Subject.java
+++ src/main/java/location/app/vehicule_location_app/observer/Subject.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 
 public class Subject{
 
+    private static Subject instance;
+
+    protected Subject() {}
+
+    public static Subject getInstance() {
+        if (instance == null) {
+            instance = new Subject();
+        }
+        return instance;
+    }
+
     private ArrayList<Observer> observers = new ArrayList<Observer>();
     public void attach(Observer observer) {
         observers.add(observer);
